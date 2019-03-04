451e20d4b36e77707b4920a0d67f63636dbae6a1
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-04 11:26:45 -0500

    first attempt inventory

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -1,37 +1,40 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Use a map to keep track of inventory in a store
  */
 public class Inventory {
+    private List<String > strings;
     /**
      * @param strings list of strings to add / remove / fetch from
      */
     public Inventory(List<String> strings) {
-
+        this.strings = strings;
     }
 
     /**
      * nullary constructor initializes a new list
      */
     public Inventory() {
-
+        this.strings = null;
     }
 
     /**
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
-        return;
+        strings.add(item);
     }
 
     /**
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
-        return;
+        strings.remove(item);
     }
 
     /**
@@ -39,6 +42,10 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+        if (strings.contains(item)) {
+            return Collections.frequency(strings, item);
+        } else {
+            return 0;
+        }
     }
 }
